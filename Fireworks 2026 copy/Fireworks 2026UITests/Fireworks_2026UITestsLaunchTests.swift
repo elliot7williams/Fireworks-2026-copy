76bcf5159b03be0ede97166df5745572e296075b
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:28:51 -0400

    Add Fireworks 2026 copy/Fireworks 2026UITests/Fireworks_2026UITestsLaunchTests.swift

---
+++ Fireworks 2026 copy/Fireworks 2026UITests/Fireworks_2026UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fireworks_2026UITestsLaunchTests.swift
+//  Fireworks 2026UITests
+//
+//  Created by Elliot Williams on 2025-06-04.
+//
+
+import XCTest
+
+final class Fireworks_2026UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
