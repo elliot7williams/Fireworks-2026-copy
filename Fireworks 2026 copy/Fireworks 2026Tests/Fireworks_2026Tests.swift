880e87c7a35432fc9df7085194e4916b241d641e
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:29:17 -0400

    Add Fireworks 2026 copy/Fireworks 2026Tests/Fireworks_2026Tests.swift

---
+++ Fireworks 2026 copy/Fireworks 2026Tests/Fireworks_2026Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Fireworks_2026Tests.swift
+//  Fireworks 2026Tests
+//
+//  Created by Elliot Williams on 2025-06-04.
+//
+
+import Testing
+@testable import Fireworks_2026
+
+struct Fireworks_2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
